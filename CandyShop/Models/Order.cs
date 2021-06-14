ad12e2a962819bbbc619ea2c70df781918a072a1
eylem abuzeyitoğlu <abuzeyitoglu.eylem@gmail.com>
2021-06-14 22:28:20 +0300

    Added Order Form

---
+++ CandyShop/Models/Order.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandyShop.Models
+{
+    public class Order
+    {
+        [BindNever]
+        public int OrderId { get; set; }
+
+        [Required(ErrorMessage ="Please Enter First Name")]
+        [Display(Name ="FirstName")]
+        [StringLength(25)]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Last Name")]
+        [Display(Name = "LastName")]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Address")]
+        [Display(Name = "Street Address")]
+        [StringLength(100)]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Please Enter City")]
+        public string City { get; set; }
+
+
+        [Required(ErrorMessage = "Please Enter State")]
+        [StringLength(2,MinimumLength =2)]
+        public string State { get; set; }
+
+
+        [Required(ErrorMessage = "Please Enter Zipcode")]
+        [StringLength(5, MinimumLength = 5)]
+        public string ZipCode { get; set; }
+
+
+        [Required(ErrorMessage = "Please Enter PhoneNumber")]
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+
+
+        public List<OrderDetail> OrderDetails { get; set; }
+
+        [BindNever]
+        public decimal OrderTotal { get; set; }
+        [BindNever]
+        public DateTime OrderPlaced { get; set; }
+    }
+}
